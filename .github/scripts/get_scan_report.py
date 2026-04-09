87e116386eadf254eeb78b3310cee5afcfa9f159
matthew20103 <ma77hew20103@gmail.com>
2026-04-09 09:59:55 +0800

    Update get_scan_report.py

--- .github/scripts/get_scan_report.py
+++ .github/scripts/get_scan_report.py
@@ -114,9 +114,9 @@ def fetch_full_report_suite(job_id, base_endpoint, title, scan_type):
             
             # Map for Mermaid Colors and Sorting Order
             severity_colors = {
-                "CRITICAL": "#FF0000",   # Dark Red
+                "CRITICAL": "#FFA500",   # Dark Red
                 "HIGH": "#8B0000",       # Red
-                "MEDIUM": "#FFA500",     # Orange
+                "MEDIUM": "#FF0000",     # Orange
                 "LOW": "#FFFF00",        # Yellow
                 "INFORMATIONAL": "#ADD8E6" # Light Blue
             }
